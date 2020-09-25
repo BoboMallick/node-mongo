651e5ab05c500cc837b0fa0f66b620ffb3c08c2d
bobo mallick <mallickbobo@gmail.com>
2020-09-26 00:58:21 +0530

    done

--- index.js
+++ index.js
@@ -3,10 +3,26 @@ const express = require('express');
 
 const app = express();
 
+const users = ["katha", "shri", "bobo"];
+
 
 app.get('/', (req, res) => {
-    res.send('hello world');
+    const fruit = {
+        products: 'banana',
+        prices: 10
+    }
+    res.send(fruit);
+  });
+
+  app.get('/fruit/mango', (req, res) => {
+      res.send({fruit: 'mango'});
+  });
+
+  app.get('/users/:id', (req, res) => {
+      const id = req.params.id;
+      const name = users[id];
+      res.send({id, name});
   })
-  
+
   
   app.listen(3000, () => console.log('Listening to port 3000'));
